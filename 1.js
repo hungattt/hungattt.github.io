1c9ec831a1105ff5a71b84a692043d004f998fa4
hungattt <50527173+hungattt@users.noreply.github.com>
2021-02-04 16:43:40 +0700

    up

--- 1.js
+++ 1.js
@@ -1,4 +1,12 @@
-﻿ $(function(){
- 
-})  
- 
+﻿$(function () {
+    // vitricuaedu = $('#khoiedu').offset().top;
+    //console.log(vitricuaedu);
+
+    $(".xuong,.kn").click(function () {
+        $('body,html').animate({ scrollTop: $('#khoiedu').offset().top }, 900)
+    })
+
+    $(".sp").click(function () {
+        $('body,html').animate({ scrollTop: $('#khoisanpham').offset().top }, 900)
+    })
+})
